5eeb2e2435957c6d20a7962bc7a1a58c3d9716d5
catcarbonell <hello@catcarbn.com>
2020-09-07 19:58:45 -0700

    Fixed entries styles

--- components/cover-image.js
+++ components/cover-image.js
@@ -5,7 +5,7 @@ export default function CoverImage({ title, url, slug }) {
     <img
       src={url}
       alt={`Cover Image for ${title}`}
-      className="h-xxs w-full object-cover rounded-xl"
+      className="h-xxs w-full object-cover"
     />
   )
   return (
