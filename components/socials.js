d40d993c87d487d5f6a4e2244aabf8b3ee465430
catcarbonell <hello@catcarbn.com>
2020-09-07 17:46:20 -0700

    fixed social media icons

--- components/socials.js
+++ components/socials.js
@@ -1,11 +1,14 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import {faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Socials = () => {
     return(
-        <div className="text-custom-violet">
+        <div className="flex flex-row content-evenly text-custom-violet">
             <a href="https://twitter.com/catcarbn" target="new">
-            <FontAwesomeIcon icon={faTwitter} className="w-16" size="sm" />
+                <FontAwesomeIcon icon={faTwitter} className="w-8 mr-4" size="sm" />
+            </a>
+            <a href="https://linkedin.com/in/catcarbonell" target="new">
+                <FontAwesomeIcon icon={faLinkedin} className="w-8" size="sm" />
             </a>
         </div>
     )
